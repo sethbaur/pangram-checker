a694db7fe884eb2e0c46a8c722c188636813896f
Seth <sethbaur@gmail.com>
2019-05-30 10:15:16 -0400

    Fixing styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "../styles/layout.scss"
+import styles from "../styles/layout.module.scss"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -27,12 +27,7 @@ const Layout = ({ children }) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
+          className={styles.container}
         >
           <main>{children}</main>
           <footer>
